84c90f6f94c1e68924baefa80ab6a194ff22c606
Jean Yang <jean.yang.writeme@gmail.com>
2014-10-05 00:58:14 -0400

    allow people to figure out what month now

--- GetPage.py
+++ GetPage.py
@@ -1,7 +1,33 @@
 import urllib2
 
-def get_page_contents():
-    req = urllib2.Request('https://lists.csail.mit.edu/pipermail/csail-related/2014-September.txt')
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    return the_page.splitlines()
+BASE_URL = "https://lists.csail.mit.edu/pipermail/csail-related/" 
+MONTHS = ["January", "February", "March", "April", "May", "June", "July"
+    , "August", "September", "October", "November", "December"]
+
+def get_page_contents(num_months):
+    # The first available archive is July 2004.
+    cur_month = 9 # TODO: Get this from time-date.
+    cur_year = 2014
+
+    lines = []
+
+    for i in range(0, num_months):
+        # Hacky hard-coded stop at current year.
+        if cur_month == 6 and cur_year == 2004:
+            break
+
+        archive_url = BASE_URL + str(cur_year) + "-" + MONTHS[cur_month] + \
+            ".txt"
+        print ("Fetching " + archive_url + "...")
+        req = urllib2.Request(archive_url)
+        response = urllib2.urlopen(req)
+        the_page = response.read()
+        lines.extend(the_page.splitlines())
+
+        if cur_month == 0:
+            cur_month = 11
+            cur_year = cur_year - 1
+        else:
+            cur_month = cur_month - 1
+
+    return lines
--- scrape.py
+++ scrape.py
@@ -11,23 +11,26 @@ CSAIL_RELATED_URL = "https://lists.csail.mit.edu/pipermail/csail-related/"
 
 def main(argv):
     email_of_interest = ''
-    outputfile = ''
+    outputfile = 'out.txt'
+    months = 1
+
     try:
-        opts, args = getopt.getopt(argv,"he:o:",["email=","ofile="])
+        opts, args = getopt.getopt(argv,"he:o:m:",["email=","ofile=","months="])
     except getopt.GetoptError:
-        print 'scrape.py -e <email> -o <outputfile>'
+        print 'scrape.py -e <email> -o <outputfile> -m <months>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'scrape.py -e <email> -o <outputfile>'
+            print 'scrape.py -e <email> -o <outputfile> -m <months>'
             sys.exit()
         elif opt in ("-e", "--email"):
             email_of_interest = arg.replace("@", " at ")
         elif opt in ("-o", "--ofile"):
             outputfile = arg
+        elif opt in ("-m", "--months"):
+            months = int(arg)
 
-    # TODO: Read args eventually.
-    page_lines = GetPage.get_page_contents()
+    page_lines = GetPage.get_page_contents(months)
     emails = ProcessContents.process_contents(page_lines, email_of_interest)
     
     f = open(outputfile, 'w')
